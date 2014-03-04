ec94212214db1d5ca954530839ab6cec239961bc
Matt Thomas <matt@platformprojects.org>
2014-03-04 14:47:24 +0000

    Added code to 'fix' bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -4,26 +4,24 @@ class Bike
 # you create a new class by typing Bike.new
 
 	def initialize
-
 		# All instance variables begin with @
-
 		# This needs to be an instance variable
 		# because we'll need it in other methods
-
 		@broken = false
 	end
 
 	def broken?
 		# instance variables are avaible in all methods
-
 		@broken
 	end
 
 	def break
-		
 		# AND any instance can update them!
-
 		@broken = true
+	end
+
+	def fix
+		@broken = false
 	end
 
 end
